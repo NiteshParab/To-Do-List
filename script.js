c037a88d63a9630d6b4969527212441fea8df754
NiteshParab <158831334+NiteshParab@users.noreply.github.com>
2024-02-12 20:15:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputBox=document.querySelector(".inputBox");
+let addButton=document.querySelector(".addButton");
+let radioButton=document.querySelector(".radioButton");
+let goal=document.querySelector(".goal");
+let checkBox=document.querySelector(".checkBox");
