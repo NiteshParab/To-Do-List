da0566a87ca88558f4ed58310df02b9a7f628053
NiteshParab <158831334+NiteshParab@users.noreply.github.com>
2024-02-13 19:08:03 +0530

    added functionality
    added functionality

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let inputBox=document.querySelector(".inputBox");
 let addButton=document.querySelector(".addButton");
-let radioButton=document.querySelector(".radioButton");
+let listContainer=document.querySelector(".list-container");
 let goal=document.querySelector(".goal");
 let checkBox=document.querySelector(".checkBox");
+
+
+addButton.addEventListener("click",()=>{
+    let newBox=`<div class="box">
+                    <input type="checkbox" class="checkBox">
+                    <p class="goal">${inputBox.value}</p>
+                    <input type="checkbox" class="checkBox">
+                </div>`;
+    
+    if(inputBox.value!=""){
+        listContainer.innerHTML=listContainer.innerHTML+newBox;
+        inputBox.value="";
+        goal.classList.toggle("strike")
+    }
+    else{
+        alert("please enter the Goal...");
+    }
+});
+
+
+
