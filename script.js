7f42a39e1ac8a1c1c7b25b79869a8d5fd7635dfe
NiteshParab <158831334+NiteshParab@users.noreply.github.com>
2024-02-15 11:26:17 +0530

    add new images
    add new images

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 let inputBox=document.querySelector(".inputBox");
 let addButton=document.querySelector(".addButton");
 let listContainer=document.querySelector(".list-container");
-let goal=document.querySelector(".goal");
-let checkBox=document.querySelector(".checkBox");
-let clear=document.querySelector(".clear");
+let task=document.querySelector(".task");
 
 
-addButton.addEventListener("click",()=>{
-    let newBox=`<div class="box">
-                    <input type="checkbox" class="checkBox">
-                    <p class="goal">${inputBox.value}</p>
-                    <button class="clear"><i class="fa-regular fa-rectangle-xmark"></i></button>
-                </div>`;
-                
+addButton.addEventListener("click",(e)=>{      
     if(inputBox.value!=""){
-        listContainer.innerHTML=listContainer.innerHTML+newBox;
+        let li=document.createElement("li");
+        li.innerText=inputBox.value;
+        task.append(li);
         inputBox.value="";
-        goal.classList.toggle("strike")
     }
     else{
         alert("please enter the Goal...");
     }
+    e.stopPropagation();
 });
+
+
+
+
 
 
 
